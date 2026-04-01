173a7b750f4aacc9b3cf26daec2a975f966f1c2d
˶𖥦 <rhyme.qing@gmail.com>
2026-04-01 16:43:27 +0800

    docs: add new content

--- src/data/comics/index.ts
+++ src/data/comics/index.ts
@@ -3,9 +3,11 @@ export type { ComicBubble, ComicTranslation, Comic } from "./types";
 import { tuesdayPhoto } from "./tuesday-photo";
 import { clownfish } from "./clownfish";
 import { sourcePhoto } from "./square-cat";
+import { bloomerPhoto } from "./late-bloomer";
 
 export const comics = [
   tuesdayPhoto,
   clownfish,
-  sourcePhoto
+  sourcePhoto,
+  bloomerPhoto
 ];
